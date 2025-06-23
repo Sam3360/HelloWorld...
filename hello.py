daccc9537543e8690f26e70f3e78c7b5b6ba1517
Sam3360 <iforgot3360@gmail.com>
2025-06-23 18:47:13 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,18 @@
+print("Hello there!")
+print("I'm a simple program, and I'd love to get to know you a little better.....")
+print("My name is Info-Gatherer 3000")
+print("What's your name?")
+
+user_name = input("Please type your name and press Enter: ")
+
+print("---------------------------------------------------------------------------------------------------------------------------------------------------------------")
+print("It's great to meet you, " + user_name + "!")
+
+print("Now, let's try something else.")
+favorite_color = input("What's your favorite color? ")
+
+print("Ah, " + favorite_color + " is a fantastic color!")
+print(".---------------------------------------------------------------------------------------------------------------------------------------------------------------")
+
+print("Thanks for chatting with me, " + user_name + "!")
+print("See you around!")
